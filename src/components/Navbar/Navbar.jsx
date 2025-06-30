ce6e6051143ef2c08b9d3c13a64a33bd8d44af00
Deependra <deependrapratapsingh855@gmail.com>
2025-06-30 13:55:08 +0530

    nothing

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -29,7 +29,7 @@ const Navbar = () => {
         setIsOpen(false);
       }
     };
-
+ 
     window.addEventListener('scroll', handleScroll);
     window.addEventListener('resize', handleResize);
     
